3f41d083da437b2241f3a64e813716827622d49c
David E. Wheeler <david@justatheory.com>
2011-09-08 15:40:07 -0700

    Increment to v0.16.1.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-PGXN/API version 0.16.0
+PGXN/API version 0.16.1
 ======================
 
 This application provides a REST API for flexible searching of PGXN
--- lib/PGXN/API/Router.pm
+++ lib/PGXN/API/Router.pm
@@ -12,7 +12,7 @@ use Plack::Request;
 use Encode;
 use File::Spec::Functions qw(catdir);
 use namespace::autoclean;
-our $VERSION = v0.16.0;
+our $VERSION = v0.16.1;
 
 sub app {
     my ($class, %params) = @_;
